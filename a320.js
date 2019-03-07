439905efa6e531278508d23d1671fe5ea0a5b975
rushikc <1ms16cs021@gmail.com>
2019-03-07 15:07:23 +0530

    updated

---
+++ a320.js
@@ -0,0 +1,26 @@
+
+
+
+function upload() {
+
+
+
+const firebase = require('firebase')
+const config = {
+    apiKey: "AIzaSyChU9Ot8MCdRojD5IfmJKgYCAtAM4C-bu0",
+    authDomain: "aerothon-5a1e4.firebaseapp.com",
+    databaseURL: "https://aerothon-5a1e4.firebaseio.com",
+    projectId: "aerothon-5a1e4",
+    storageBucket: "aerothon-5a1e4.appspot.com",
+    messagingSenderId: "647231323700"
+  };
+  firebase.initializeApp(config);
+const ref = firebase.database();
+
+
+
+
+
+
+
+}
